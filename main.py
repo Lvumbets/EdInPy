428166b64e0f62774ba27f29a3daaecce12fc7f2
Lvumbets <leva-lev2010@mail.ru>
2025-04-06 01:23:41 +0300

    1 develop commit
    add:
    base.html

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return f'<h1>Hello world!</h1>'
-
-
+    return render_template('base.html', title='Шаблон')
 
 
 def main():
